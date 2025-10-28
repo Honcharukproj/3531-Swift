f8e7037fad05a7ea6304a2ec4ebcb7c0883b15a7
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-10-28 16:59:50 +0200

    init

---
+++ Pods/Target Support Files/Pods-RunawayRooster/Pods-RunawayRooster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RunawayRooster : NSObject
+@end
+@implementation PodsDummy_Pods_RunawayRooster
+@end
---
+++ RunawayRooster/App/RunawayRoosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunawayRoosterApp.swift
+//  RunawayRooster
+//
+//  Created by Serhii Babchuk on 28.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct RunawayRoosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RRLaunchView()
+        }
+    }
+}
